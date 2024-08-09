89d9b35f7d7a5319ddf9152791293c1e2be2ca20
Jakub Jaskulski <jakub.jaskulski.it@gmail.com>
2024-08-09 08:56:48 +0200

    Improve Header Search
    - add popup when search is focused
    - split header css to separate file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,66 @@
+import './style.css';
+import {useState} from "react";
+
+
 export const Header = () => {
     return (
         <div className="header">
-            <div className='headerSearch'>Search</div>
-            <div className='feedTypeSelection'>
-                <div className='feedTypeSelectionButton'>1</div>
-                <div className='feedTypeSelectionButton'>2</div>
-                <div className='feedTypeSelectionButton'>3</div>
-            </div>
-            <div className='headerRoundIconMenu'>
-                <div className='roundIconButton'>M</div>
-                <div className='roundIconButton'>N</div>
-                <div className='roundIconButton'>A</div>
-            </div>
+            <Search/>
+            <FeedTypeSelection/>
+            <RoundIconMenu/>
+        </div>
+    )
+}
+
+const Search = () => {
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+    const handleFocus = () => {
+        setIsPopupVisible(true);
+    };
+
+    const handleBlur = () => {
+        setIsPopupVisible(false)
+    };
+    return (
+        <div className='headerSearch'>
+            <input type="text" placeholder='Search' onFocus={handleFocus}
+                   onBlur={handleBlur}
+
+            />
+            {isPopupVisible && (
+                <PreviousSearchesPanel/>
+            )}
+        </div>
+    )
+}
+
+const PreviousSearchesPanel = () => {
+    return (
+        <div className='previousSearchesPanel'>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </div>
+    )
+}
+
+const FeedTypeSelection = () => {
+    return (
+        <div className='feedTypeSelection'>
+            <div className='feedTypeSelectionButton'>1</div>
+            <div className='feedTypeSelectionButton'>2</div>
+            <div className='feedTypeSelectionButton'>3</div>
+        </div>
+    )
+}
+
+const RoundIconMenu = () => {
+    return (
+        <div className='headerRoundIconMenu'>
+            <div className='roundIconButton'>M</div>
+            <div className='roundIconButton'>N</div>
+            <div className='roundIconButton'>A</div>
         </div>
     )
 }

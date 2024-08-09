135afd402e9806438c2e740f0a93f8cb6b956bad
Jakub Jaskulski <jakub.jaskulski.it@gmail.com>
2024-08-09 08:56:34 +0200

    Initial html design

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+function App() {
+    return (
+        <section className="layout">
+            <div className="header">
+                <div className='headerSearch'>Search</div>
+                <div className='feedTypeSelection'>
+                    <div className='feedTypeSelectionButton'>1</div>
+                    <div className='feedTypeSelectionButton'>2</div>
+                    <div className='feedTypeSelectionButton'>3</div>
+                </div>
+                <div className='headerRoundIconMenu'>
+                    <div className='roundIconButton'>M</div>
+                    <div className='roundIconButton'>N</div>
+                    <div className='roundIconButton'>A</div>
+                </div>
+            </div>
+            <div className="leftSide">
+                <div className="shortcut">
+                    123
+                </div>
+                <div className="shortcut">
+                    123
+                </div>
+                <div className="shortcut">
+                    123
+                </div>
+            </div>
+            <div className="body">
+                <div className="feed">
+                    <div className="post">
+                        postpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpostpost
+                    </div>
+                    <div className="post">
+                        postpostpost
+                    </div>
+                    <div className="post">
+                        postpostpost
+                    </div>
+                    <div className="post">
+                        postpostpost
+                    </div>
+                    <div className="post">
+                        postpostpost
+                    </div>
+                </div>
+            </div>
+            <div className="rightSide">
+                <div className="contacts">
+                    <div className="contactsHeader">
+                        Contacts
+                    </div>
+                    <div className="contact">
+                        Test Testington
+                    </div>
+                    <div className="contact">
+                        Teston Testingsky
+                    </div>
+                </div>
+            </div>
+            <div className="footer">5</div>
+        </section>
+    )
+}
+
+export default App;
